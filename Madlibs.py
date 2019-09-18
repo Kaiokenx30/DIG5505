2007bb6ea0048de8ed30e2cfcb2e284e577f40f3
Kaiokenx30 <alexander.boyd@ucf.edu>
2019-09-18 19:15:32 -0400

    First upload!

---
+++ Madlibs.py
@@ -0,0 +1,19 @@
+import random 
+#%%
+places = ["Peru","England","Florida"]
+things = ['Shoe', 'Broom', 'Pillow']
+state = ['Fright','Delight','Stuper']
+time = ['Night','Moon','Dawn']
+truth = ['Come True','Been misconstrued','Been devined by the all gods as an omen of his grizzly fate.']
+
+print("There once was a man from ",random.choice(places))
+
+print('Who drempt he was eating his ',random.choice(things))
+
+print('He woke up in a ',random.choice(state))
+
+print('In the dark of the ')
+
+print('And found that his dream had ',random.choice(truth))
+
+#%%
